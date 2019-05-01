9d07dc53b48c6106ff79ffd381626c927a1a789c
shinzm01 <shane.hinzman@fiscal.treasury.gov>
2019-05-01 14:15:38 -0400

    Added front end get flights

--- src/main/java/com/chikli/demo/Main.java
+++ src/main/java/com/chikli/demo/Main.java
@@ -9,6 +9,14 @@ import java.util.List;
 public class Main {
 
     public static void main(final String[] args) throws IOException {
+        Flight flight1 = new Flight("CAK Akron, OH", "CLE Cleveland, OH", "05/01/2019", "8:00 AM", "8:15 AM", "0h 15m", 29.00);
+        Flight flight2 = new Flight("CMH Columbus, OH", "MIA Miami, FL", "05/01/2019", "9:00 AM", "12:05 PM", "3h 15m", 149.00);
+        Flight flight3 = new Flight("PIT Pittsburgh, PA", "TPA Tampa, FL", "05/01/2019", "2:15 PM", "4:55 PM", "2h 40m", 99.00);
+
+        Flight.addFlights(flight1);
+        Flight.addFlights(flight2);
+        Flight.addFlights(flight3);
+
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         boolean run = true;
 
@@ -26,6 +34,14 @@ public class Main {
 
             if (input == 0){
                 run = false;
+            }
+
+            else if (input == 1){
+                List<Flight> allFlights = Flight.getFlights();
+
+                for(int i = 0; i < allFlights.size(); i++){
+                    System.out.println("Departure city: " + allFlights.get(i).getDepartureCity());
+                }
             }
 
             else{
